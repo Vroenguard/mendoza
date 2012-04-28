ecca8f1738b6f7cd0b215e0557a9ca8478144633
Antoine Luciani <antoine1fr@gmail.com>
2012-04-28 17:30:32 +0200

    Implement the constructor of net::ATcpServer.

---
+++ common/include/ATcpServer.hh
@@ -0,0 +1,29 @@
+//==============================================================================
+// @author:	antoine luciani <antoine1fr@gmail.com>
+// File:	ATcpServer.hh
+// Creation:	Sat Apr 28 16:54:02 2012
+//==============================================================================
+
+#pragma once
+
+#include "LblibSafeQueue.hpp"
+
+namespace threads = lblib::threads;
+
+namespace net
+{
+  class ATcpServer
+  {
+    private:
+      threads::SafeQueue<int>	_queue;
+      size_t			_maxConnections;
+      int			_socket;
+
+    public:
+      typedef unsigned short Port;
+      ATcpServer(Port, size_t maxConnections);
+
+    private:
+      bool _bind(Port);
+  }; // ATcpServer
+} // net
---
+++ common/src/ATcpServer.cpp
@@ -0,0 +1,45 @@
+//==============================================================================
+// @author:	antoine luciani <antoine1fr@gmail.com>
+// File:	ATcpServer.cpp
+// Creation:	Sat Apr 28 17:00:20 2012
+//==============================================================================
+
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "ATcpServer.hh"
+
+namespace net
+{
+  ATcpServer::ATcpServer(Port port, size_t maxConnections)
+    : _maxConnections(maxConnections)
+  {
+    _socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (_socket == -1)
+    {
+      int option = 1;
+      setsockopt(_socket, SOL_SOCKET, SO_REUSEADDR, &option, sizeof(option));
+      if (_bind(port))
+      {
+	close(_socket);
+	_socket = -1;
+      }
+    }
+  }
+
+  bool ATcpServer::_bind(Port port)
+  {
+    sockaddr_in sockAddr;
+    sockAddr.sin_family = AF_INET;
+    sockAddr.sin_port = htons(port);
+    sockAddr.sin_addr.s_addr = INADDR_ANY;
+    if (bind(_socket, reinterpret_cast<sockaddr*>(&sockAddr),
+	  sizeof(sockAddr)) != -1)
+    {
+      return true;
+    }
+    return false;
+  }
+} // net
