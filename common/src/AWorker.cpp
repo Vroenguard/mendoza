d96094677742aaff87fac41e3d2afeff9b585e61
Antoine Luciani <antoine1fr@gmail.com>
2012-04-28 18:29:01 +0200

    Implement constructor and destructor of net::AWorker.

---
+++ common/src/AWorker.cpp
@@ -0,0 +1,20 @@
+//==============================================================================
+// @author:	antoine luciani <antoine1fr@gmail.com>
+// File:	AWorker.cpp
+// Creation:	Sat Apr 28 18:21:58 2012
+//==============================================================================
+
+#include "AWorker.hh"
+
+namespace net
+{
+  template <typename T>
+    AWorker<T>::AWorker(void)
+    {
+    }
+
+  template <typename T>
+    AWorker<T>::~AWorker(void)
+    {
+    }
+} // net
