fc442876d49501b5454eba82594f1fdf8b98dd51
Antoine Luciani <antoine1fr@gmail.com>
2012-04-29 13:18:58 +0200

    mailbox_create also takes a password in parameters from now on.

--- mailbox_creator/main.cpp
+++ mailbox_creator/main.cpp
@@ -6,17 +6,21 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cstring>
 
 #include "MailBox.hh"
 
 int main(int argc, char** argv)
 {
-  if (argc != 2)
+  if (argc != 3)
   {
-    std::cerr << "Usage: " << argv[0] << " account_name\n";
+    std::cerr << "Usage: " << argv[0] << " login password\n";
     return EXIT_FAILURE;
   }
+  mail::MailBox::Password password;
+  strncpy(password, argv[2], sizeof(password));
   mail::MailBox mailBox(argv[1], "");
+  mailBox.setPassword(password);
   mailBox.write();
   return EXIT_SUCCESS;
 }
